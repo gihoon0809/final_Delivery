919c7fbc402570d3b830b9309c35ba8ca38c5ffd
gihoon0809 <gihoon0809@gmail.com>
2020-11-04 01:20:49 +0900

    first

---
+++ src/main/java/newpizza/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package newpizza;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
+
+
+}
