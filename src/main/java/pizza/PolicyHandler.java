ca882f92973aad0dda602d45b3678a56eb887156
gihoon0809 <gihoon0809@gmail.com>
2020-11-04 14:53:48 +0900

    upper

--- src/main/java/pizza/PolicyHandler.java
+++ src/main/java/pizza/PolicyHandler.java
@@ -10,6 +10,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
+
+    // checkpoint1. SAGA pub/sub LDH 202011041103
+    // [소스추가] Autowired
+    @Autowired
+    DeliveryRepository deliveryRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
     public void onStringEventListener(@Payload String eventString){
 
@@ -19,6 +25,14 @@ public class PolicyHandler{
     public void wheneverPaid_RequestDelivery(@Payload Paid paid){
 
         if(paid.isMe()){
+
+            // checkpoint1. SAGA pub/sub LDH 202011041103
+            // [소스추가] payment 정보 delivery에 등록
+            Delivery delivery = new Delivery();
+            delivery.setOrderId(paid.getOrderId());
+            delivery.setDeliveryStatus("Delivered");
+            deliveryRepository.save(delivery);
+
             System.out.println("##### listener RequestDelivery : " + paid.toJson());
         }
     }
